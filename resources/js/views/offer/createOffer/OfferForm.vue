25b0dc7a37fe54cc91c1b61323995719b2af1c63
linhlt311 <ltlinh311@gmail.com>
2019-10-16 13:20:03 +0700

    add all step in 1 create offer screen

--- resources/js/views/offer/createOffer/OfferForm.vue
+++ resources/js/views/offer/createOffer/OfferForm.vue
@@ -1,169 +1,157 @@
 <template>
-  <div>
-    <section class="content-header">
-      <div class="row">
-        <div class="col col-lg-6">
-          <h1 class="title">Create new Offer</h1>
-        </div>
-        <div class="col col-lg-6">
-          <el-input
-            placeholder="Search Offer, Group ..."
-            prefix-icon="el-icon-search"
-            suffix-icon="el-icon-close"
-            v-model="input2"
-          ></el-input>
-        </div>
-      </div>
-    </section>
-    <section class="content">
-      <div class="container">
-        <div class="row link">
-          <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb-link">
-            <el-breadcrumb-item style="font-weight: bold">Create new offer</el-breadcrumb-item>
-            <el-breadcrumb-item>Location</el-breadcrumb-item>
-            <el-breadcrumb-item>Offer Settings</el-breadcrumb-item>
-          </el-breadcrumb>
-        </div>
-        <div class="row div-content">
-          <h2>Choose your objective</h2>
-        </div>
-        <div class="row row-center">
-          <!-- Basic upsell -->
-          <div class="col-md-4 col-sm-12">
-            <offer-card :data="upSellBasic"></offer-card>
-          </div>
-          <!-- Basic Crosssell -->
-          <div class="col-md-4 col-sm-12">
-            <offer-card :data="upSellBasic"></offer-card>
-          </div>
-        </div>
-        <div class="row row-center top-buffer ">
-          <!-- Upsell with boost -->
-          <div class="col-md-4 col-sm-12">
-            <offer-card
-              :data="upSellBoost"
-              :isUpSellBoost="isUpSellBoost"
-              :isCrossSellBoost="isCrossSellBoost"
-              @btnClicked="updateStatus"
-            ></offer-card>
-          </div>
-          <!-- Cross sell with boost -->
-          <div class="col-md-4 col-sm-12 col-12">
-            <offer-card
-              :data="crossSellBoost"
-              :isUpSellBoost="isUpSellBoost"
-              :isCrossSellBoost="isCrossSellBoost"
-              @btnClicked="updateStatus"
-            ></offer-card>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+    <div class="main-container choose-offer-type">
+        <section class="content-header">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Create new offer</h2>
+                </div>
+            </div>
+            <div class="col-md-12" style="font-size: 16px">
+                <el-breadcrumb class="create-offer-step" separator-class="el-icon-arrow-right">
+                    <el-breadcrumb-item><strong>Offer Type</strong></el-breadcrumb-item>
+                    <el-breadcrumb-item>Location</el-breadcrumb-item>
+                    <el-breadcrumb-item>Offer settings</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
+        </section>
+        <section class="content">
+			<div class="row div-content">
+				<h2>Choose your objective</h2>
+			</div>
+			<div class="row">
+				<!-- Basic upsell -->
+				<div class="col-md-2"></div>
+				<div class="col-md-4 col-sm-12">
+					<offer-card :data="upSellBasic" :step="step" @nextStep='next'></offer-card>
+				</div>
+				<!-- Basic Crosssell -->
+				<div class="col-md-4 col-sm-12">
+					<offer-card :data="upSellBasic" :step="step" @nextStep='next'></offer-card>
+				</div>
+			</div>
+			<div class="row top-buffer ">
+				<div class="col-md-2"></div>
+				<!-- Upsell with boost -->
+				<div class="col-md-4 col-sm-12">
+					<offer-card :data="upSellBoost" :isUpSellBoost="isUpSellBoost" :isCrossSellBoost="isCrossSellBoost" @btnClicked="updateStatus"></offer-card>
+				</div>
+				<!-- Cross sell with boost -->
+				<div class="col-md-4 col-sm-12 col-12">
+					<offer-card :data="crossSellBoost" :isUpSellBoost="isUpSellBoost" :isCrossSellBoost="isCrossSellBoost" @btnClicked="updateStatus"></offer-card>
+				</div>
+			</div>
+        </section>
+    </div>
 </template>
 
 <script>
-import OfferCard from "./OfferCard.vue";
+    import OfferCard from "./OfferCard.vue";
 
-export default {
-  data() {
-    return {
-      isUpSellBoost: false,
-      isCrossSellBoost: false,
-      upSellBasic: {
-        image: "https://via.placeholder.com/300x190",
-        description:
-          "Encouraging the purchase of anything in conjunction with the primary product",
-        id: "upsell",
-        btnValue: "Basic Upsell",
-        isBoost: false
-      },
-      upSellBoost: {
-        image: "https://via.placeholder.com/300x190",
-        description: "Create urgencies with boosters",
-        id: "upsell",
-        isBoost: true
-      },
-      crossSellBoost: {
-        image: "https://via.placeholder.com/300x190",
-        description: "Create urgencies with boosters",
-        id: "crosssell",
-        isBoost: true
-      }
+    export default {
+		props: ["step"],
+        data() {
+                return {
+                    isUpSellBoost: false,
+                    isCrossSellBoost: false,
+                    upSellBasic: {
+                        image: "https://via.placeholder.com/300x190",
+                        description: "Encouraging the purchase of anything in conjunction with the primary product",
+                        id: "upsell",
+                        btnValue: "Basic Upsell",
+                        isBoost: false
+                    },
+                    upSellBoost: {
+                        image: "https://via.placeholder.com/300x190",
+                        description: "Create urgencies with boosters",
+                        id: "upsell",
+                        isBoost: true
+                    },
+                    crossSellBoost: {
+                        image: "https://via.placeholder.com/300x190",
+                        description: "Create urgencies with boosters",
+                        id: "crosssell",
+                        isBoost: true
+                    }
+                };
+            },
+            methods: {
+                updateStatus: function(isUpSellBoost, isCrossSellBoost) {
+                    this.isUpSellBoost = isUpSellBoost;
+                    this.isCrossSellBoost = isCrossSellBoost;
+                },
+				next(step) {
+					this.step = step;
+					this.$emit("nextStep", this.step);
+				},
+            },
+            components: {
+                "offer-card": OfferCard
+            }
     };
-  },
-  props: {},
-  methods: {
-    updateStatus: function(isUpSellBoost, isCrossSellBoost) {
-      this.isUpSellBoost = isUpSellBoost;
-      this.isCrossSellBoost = isCrossSellBoost;
-    }
-  },
-  components: {
-    "offer-card": OfferCard
-  }
-};
 </script>
 
-<style>
-.content {
-  min-height: 800px;
+<style lang="scss" scoped>
+.choose-offer-type {
+    .content {
+        min-height: 800px;
+    }
+
+    .header {
+        margin-top: 10px;
+    }
+
+    .title {
+        width: 400px;
+        display: inline-block !important;
+        text-align: left;
+        margin-top: 0px;
+        margin: 0px;
+        line-height: 40px;
+        padding-left: 9px;
+        font-size: 44px;
+    }
+
+    .el-input {
+        line-height: 40px;
+        width: 450px;
+        margin: auto;
+    }
+
+    .el-input__inner {
+        border: none;
+        border-radius: 10px;
+    }
+
+    .breadcrumb {
+        background-color: transparent;
+    }
+
+    .link {
+        margin-left: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        line-height: 30px;
+    }
+
+    .breadcrumb-link {
+        font-size: 24px;
+    }
+
+    .div-content {
+        width: 300px;
+        margin: 0 auto;
+        margin-bottom: 40px;
+    }
+
+    .top-buffer {
+        margin-top: 150px;
+    }
+
+    .row-center {
+        display: flex;
+        justify-content: center;
+        flex-flow: row wrap;
+    }
 }
 
-.header {
-  margin-top: 10px;
-}
-
-.title {
-  width: 400px;
-  display: inline-block !important;
-  text-align: left;
-  margin-top: 0px;
-  margin: 0px;
-  line-height: 40px;
-  padding-left: 9px;
-  font-size: 44px;
-}
-
-.el-input {
-  line-height: 40px;
-  width: 450px;
-  margin: auto;
-}
-
-.el-input__inner {
-  border: none;
-  border-radius: 10px;
-}
-
-.breadcrumb {
-  background-color: transparent;
-}
-
-.link {
-  margin-left: auto;
-  margin-top: 40px;
-  margin-bottom: 40px;
-  line-height: 30px;
-}
-
-.breadcrumb-link {
-  font-size: 24px;
-}
-
-.div-content {
-  width: 300px;
-  margin: 0 auto;
-  margin-bottom: 40px;
-}
-
-.top-buffer {
-  margin-top: 150px;
-}
-
-.row-center {
-  display: flex;
-  justify-content: center;
-  flex-flow: row wrap;
-}
 </style>
--- resources/js/views/offer/index.js
+++ resources/js/views/offer/index.js
@@ -1,11 +1,15 @@
 import OfferList from './OfferList';
 import OfferSetting from './createOffer/OfferSetting';
 import OfferCreate from './createOffer/OfferCreate';
+import OfferForm from './createOffer/OfferForm';
+import TriggerLocation from './createOffer/TriggerLocation';
+import OfferNew from './createOffer/OfferNew';
 
 export default [
     {path: '/ult-upsell/offer', component: OfferList, name: 'offer.index'},
     {path: '/ult-upsell/offer/settings', component: OfferSetting, name: 'offer.setting'},
-    {path: '/admin/offer/create/step1', component: OfferForm, name: 'offer.create'},
-    {path: '/admin/offer/create/step2', component: TriggerLocation, name: 'offer.trigger'},
+    {path: '/ult-upsell/offer/create/step1', component: OfferForm, name: 'offer.create'},
+    {path: '/ult-upsell/offer/create/step2', component: TriggerLocation, name: 'offer.trigger'},
     {path: '/ult-upsell/offer/create', component: OfferCreate, name: 'offer.create'},
+    {path: '/ult-upsell/offer/new', component: OfferNew, name: 'offer.new'}
 ]
