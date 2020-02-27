7e3444d2b4d8308549687a66c3bd78353bc2edb4
hoangdz_97 <pahoangpham@gmail.com>
2020-02-27 10:46:56 +0700

    Update conversion

--- app/Http/Controllers/ConversionController.php
+++ app/Http/Controllers/ConversionController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
 use App\Helper\Helper;
 use Exception;
+use Illuminate\Support\Facades\Log;
 
 class ConversionController extends AdminController
 {
@@ -25,6 +26,12 @@ class ConversionController extends AdminController
         $this->conversion = $conversion;
     }
 
+    /**
+     * Store conversion
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -36,22 +43,36 @@ class ConversionController extends AdminController
             $this->conversion->insert(Conversion::createRecord($validatedData));
             return $this->response(true, '', []);
         } catch (Exception $e) {
+            Log::error("[API STORE CONVERSION]" . $e->getMessage());
             return $this->response(false, $e->getMessage(), []);
         }
     }
 
+    /**
+     * Get conversions by user
+     * Api: conversion/get
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getUserConversions(Request $request)
     {
-        $userId = Auth::id();
+        try{
+            $userId = Auth::id();
 
-        $conversions = $this->conversion->whereIn("offer_id", function ($query) use ($userId){
-            $query->select("id")->from("offers")->where("user_id", $userId);
-        })->get();
+            $conversions = $this->conversion->whereIn("offer_id", function ($query) use ($userId){
+                $query->select("id")->from("offers")->where("user_id", $userId);
+            })->get();
 
-        foreach ($conversions as $key => $value){
-            $conversions[$key]->time = Helper::parseDate($value->time);
+            foreach ($conversions as $key => $value){
+                $conversions[$key]->time = Helper::parseDate($value->time);
+            }
+
+            return  $this->response(true, '', $conversions);
+        }catch (Exception $exception){
+            Log::error("[API GET CONVERSION]". $exception->getMessage());
+            return $this->response(false, $exception->getMessage(), []);
         }
 
-        return $conversions;
     }
 }
